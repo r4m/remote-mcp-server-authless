342451f433a42c758c30cf09dcf838bb7fb75448
Filippo Zanella <ing.filippo.zanella@gmail.com>
2026-06-26 15:03:28 +0200

    Add SSE handling in fetch method

--- src/index.ts
+++ src/index.ts
@@ -64,6 +64,10 @@ export default {
 	fetch(request: Request, env: Env, ctx: ExecutionContext) {
 		const url = new URL(request.url);
 
+		if (url.pathname === "/sse") {
+			return MyMCP.serveSSE("/sse").fetch(request, env, ctx);
+		}
+
 		if (url.pathname === "/mcp") {
 			return MyMCP.serve("/mcp").fetch(request, env, ctx);
 		}
